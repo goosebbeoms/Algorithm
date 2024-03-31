350f208aa78fea0fcbdc6c1f23f4c7729cfdfa51
goosebbeoms <108177348+goosebbeoms@users.noreply.github.com>
2024-03-31 18:14:31 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,5 @@
+SELECT used_goods_board.title, used_goods_board.board_id, used_goods_reply.reply_id, used_goods_reply.writer_id, used_goods_reply.contents, DATE_FORMAT(used_goods_reply.created_date, '%Y-%m-%d') AS created_date
+FROM used_goods_board INNER JOIN used_goods_reply
+ON used_goods_board.board_id = used_goods_reply.board_id
+WHERE SUBSTR(used_goods_board.created_date, 1, 7) = '2022-10'
+ORDER BY used_goods_reply.created_date, used_goods_board.title;
