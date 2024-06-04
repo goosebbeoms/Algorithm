604f57179858e19692137d414454e1782dbffb4e
goosebbeoms <108177348+goosebbeoms@users.noreply.github.com>
2024-06-05 00:17:13 +0900

    [Silver III] Title: 모든 순열, Time: 220 ms, Memory: 119608 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -1,3 +1,4 @@
+'''
 from itertools import permutations
 
 N = int(input())
@@ -5,4 +6,33 @@ N = int(input())
 perms = list(permutations(list(range(1, N+1)), N))
 perms.sort()
 for item in perms:
+    print(*item)
+'''
+
+
+def perms(x):
+    global N
+
+    if x == N+1:
+        result.append(temp[:])
+        return
+
+    for i in range(1, N+1):
+        if not used[i]:
+            used[i] = True
+            temp.append(i)
+            perms(x+1)
+            used[i] = False
+            temp.pop()
+
+
+N = int(input())
+used = [False] * (N + 1)
+result = []
+temp = []
+
+perms(1)
+result.sort()
+
+for item in result:
     print(*item)
