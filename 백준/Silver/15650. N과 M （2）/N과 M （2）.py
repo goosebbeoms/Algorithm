f81ccf1c094eb1945dfec979811268f1ae1688b7
goosebbeoms <108177348+goosebbeoms@users.noreply.github.com>
2024-12-05 16:18:17 +0900

    [Silver III] Title: N과 M (2), Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,20 +1,21 @@
-def comb(x, start):
+def comb(x, num):
     if x == M:
         print(*nums)
 
-    for i in range(start, N+1):
+    for i in range(num+1, N+1):
         if used[i]:
             continue
 
         nums.append(i)
         used[i] = True
-        comb(x+1, i+1)
+
+        comb(x+1, i)
+
         nums.pop()
         used[i] = False
-
 
 N, M = map(int, input().split())
 used = [False] * (N+1)
 nums = []
 
-comb(0, 1)
+comb(0, 0)
