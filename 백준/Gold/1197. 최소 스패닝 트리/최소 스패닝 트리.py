94d5204edb1e31e935969bf7593dd03301275aa0
Gyubeom Seo <108177348+goosebbeoms@users.noreply.github.com>
2025-01-05 15:50:22 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 320 ms, Memory: 127516 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,30 +1,48 @@
-import heapq
+# 크루스칼
 
-# 프림 알고리즘
-V, E = map(int, input().split())  # V: 정점 개수, E: 간선 개수
-graph = [[] for _ in range(V+1)]
-visited = [0 for _ in range(V+1)]
+# 1. 모든 링크를 한 번에 가져온다.
+# 2. 링크를 연결하면서 같은 집합으로 만들어준다.
+# 3. 만약에 이미 같은 집합이라면 연결하지 않는다.
 
-for _ in range(E):
-    A, B, C = map(int, input().split())
-    graph[A].append([C, B])
-    graph[B].append([C, A])
+def _union(A, B):
+    A = _find(A)
+    B = _find(B)
 
+    if A == B:
+        return
+
+    if rank[A] < rank[B]:
+        parents[A] = B
+    elif rank[A] > rank[B]:
+        parents[B] = A
+    else:
+        parents[A] = B
+        rank[B] += 1
+
+
+def _find(X):
+    while X != parents[X]:
+        X = parents[X]
+
+    return X
+
+
+V, E = map(int, input().split())
+links = [list(map(int, input().split())) for _ in range(E)]
+links.sort(key=lambda x: x[2])  # 가중치 기준으로 정렬
+
+parents = [i for i in range(V+1)]
+rank = [0 for _ in range(V+1)]
 answer = 0
-cnt = 0
-# 다익스트라로 탐색
-q = [[0, 1]]  # 1에서 출발
-while q:
-    if cnt == V:
-        break
 
-    weight, node = heapq.heappop(q)
+for node1, node2, weight in links:
+    node1 = _find(node1)
+    node2 = _find(node2)
 
-    if not visited[node]:
+    if node1 == node2:
+        continue
+    else:
+        _union(node1, node2)
         answer += weight
-        cnt += 1
-        visited[node] = 1
-        for nxt in graph[node]:
-            heapq.heappush(q, nxt)
 
 print(answer)
